07b9896d4f1cd31a32aeb669ac60db740ec32e46
kazi <kazi@natitsolved.com>
2019-10-21 00:23:21 +0530

    chat attachment

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+4
+.env,3/c/3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
+?
+routes/index.js,3/0/30dfc417d69af8ed298ed4111dcab68f38ca7f54
+6
+app.js,4/a/4a68969ef8e858229267b842dedf42ab5dde4d50
--- routes/message.js
+++ routes/message.js
@@ -1,33 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const multer  =   require('multer');
 const Models = require('../models');
 const Op = require('sequelize').Op;
 
+const storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './public/files');
+  },
+  filename: function (req, file, callback) {
+    let extension = file.mimetype.split('/')
+    callback(null, file.fieldname + '-' + Date.now()+'.'+extension[1]);
+  }
+});
+const upload = multer({storage: storage});
+
 /* Message List */
 router.post('/list', async (req, res) => {
-  var payload = req.body;
+  let payload = req.body;
   try{
-    var messageList = await Models.messageList.findAll({
+    let messageList = await Models.messageList.findAll({
       where : {
         [Op.or] : [{userId : payload.userId}, {receiverId : payload.userId}]
       },
       include: [
-        { 
-          model: Models.User, 
+        {
+          model: Models.User,
           as: 'User',
           include: [
             {
-              model: Models.UserImages, 
+              model: Models.UserImages,
               as: 'Image',
             }
           ]
         },
-        { 
-          model: Models.User, 
+        {
+          model: Models.User,
           as: 'Receiver',
           include: [
             {
-              model: Models.UserImages, 
+              model: Models.UserImages,
               as: 'Image',
             }
           ]
@@ -56,13 +68,16 @@ router.post('/list', async (req, res) => {
 router.post('/details', async (req, res) => {
   let payload = req.body;
   try {
-    var messageList = await Models.message.findAll({
+    let messageList = await Models.message.findAll({
       where: {
         [Op.or]: [
           {[Op.and]: [{userId : payload.userId}, {receiverId : payload.chatUserId}]},
           {[Op.and]: [{userId : payload.chatUserId}, {receiverId : payload.userId}]}
         ]
-      }
+      },
+      order: [
+        ['id', 'DESC'],
+      ]
     });
     let response = {
       ack: 1,
@@ -83,16 +98,67 @@ router.post('/details', async (req, res) => {
 });
 
 /* Message send */
-router.post('/add', async (req, res) => {
-  var payload = req.body;
-  console.log(payload)
+router.post('/send', upload.single('attachment'), async (req, res) => {
+  let payload = req.body;
+  var data;
   try{
-    var response = Models.posts.create(payload);
-    console.log(response);
+    var payloadForMessageList;
+
+    if(payload.messageType == 'attachment') {
+      payload.attachmentLink = req.file.filename
+      let response = await Models.message.create(payload);
+      data = response.dataValues;
+      console.log("data",response)
+      payloadForMessageList = {
+        userId : payload.userId,
+        receiverId : payload.receiverId,
+        lastMessage : 'Attachment'
+      }
+    } else {
+      let response = Models.message.create(payload);
+      payloadForMessageList = {
+        userId : payload.userId,
+        receiverId : payload.receiverId,
+        lastMessage : payload.message
+      }
+    }
+    // Inserting or update the message list table
+    let messageList = await Models.messageList.findOne({
+      where : {
+        [Op.or]: [
+          {[Op.and]: [{userId : payload.userId}, {receiverId : payload.receiverId}]},
+          {[Op.and]: [{userId : payload.receiverId}, {receiverId : payload.userId}]}
+        ]
+      }
+    })
+    console.log(messageList);
+    // Message list already has entry
+    if(messageList == null) {
+      let messageListResponse = await Models.messageList.create(payloadForMessageList);
+      console.log(messageListResponse)
+    } else {
+      let messageListResponse = await Models.messageList.update(
+        { lastMessage: 'Attachment' },
+        { where: { id: messageList.dataValues.id  } }
+      );
+      console.log(messageListResponse)
+    }
+    let response = {
+      ack: 1,
+      msg: 'message send successfully',
+      data: data,
+    };
+    res.send(response);
   } catch(e) {
+    let response = {
+      ack: 0,
+      msg: 'message was not send',
+      devMsg: e,
+    };
+    res.send(response);
     console.log(e)
   }
-  res.send('sda');
 });
+
 
 module.exports = router;
